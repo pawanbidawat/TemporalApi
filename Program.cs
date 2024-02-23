4a09c44881a3f49192abe7ed48fb5af21865bcb3
pawanbidawat <94060332+pawanbidawat@users.noreply.github.com>
2024-02-23 14:23:43 +0530

    added product layoyt

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<ICategory, Category>();
+builder.Services.AddScoped<IProduct,Product>();
 
 builder.Services.AddDbContext<Context>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DataConnection")));
@@ -24,6 +25,18 @@ builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder =>
+        {
+            builder.WithOrigins("https://localhost:44366") // Replace with your frontend origin
+                   .AllowAnyHeader()
+                   .AllowAnyMethod()
+                   .WithExposedHeaders("Content-Disposition") // Add any additional exposed headers
+                   .SetIsOriginAllowed((host) => true); // Allow any origin
+        });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -32,11 +45,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("MyAllowSpecificOrigins");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+
 
 app.Run();
